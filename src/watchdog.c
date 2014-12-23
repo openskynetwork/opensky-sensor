b3e862a16178df601b93282f90062f7eaa4beb9e
Markus Engel <m_engel@cs.uni-kl.de>
2014-12-23 18:27:29 +0100

    WD: refactored code, comments

--- src/watchdog.c
+++ src/watchdog.c
@@ -1,16 +1,23 @@
 
 #include <gpio.h>
 #include <stdio.h>
+#include <unistd.h>
 
+/** Watchdog GPIO number */
 #define WD_GPIO 60
+/** Watchdog repetition rate in seconds */
 #define WD_REPEAT 30
 
+/** Initialize watchdog.
+ * \note: GPIO_init() must be called prior to that function!
+ */
 void WATCHDOG_init()
 {
 	GPIO_setDirection(WD_GPIO, GPIO_DIRECTION_OUT);
 	GPIO_clear(WD_GPIO);
 }
 
+/** Watchdog mainloop: tell the watchdog we are still here in an endless loop */
 void WATCHDOG_main()
 {
 	while (1) {
