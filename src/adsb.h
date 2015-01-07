dd08f6c22168d429e690b15555d5f78f70fda49e
Markus Engel <m_engel@cs.uni-kl.de>
2015-01-07 18:40:41 +0100

    adsb: moved options to public interface

--- src/adsb.h
+++ src/adsb.h
@@ -18,4 +18,47 @@ struct ADSB_Frame {
 void ADSB_init(const char * uart);
 void ADSB_main();
 
+/** ADSB Options */
+enum ADSB_OPTION {
+	/** Output Format: AVR */
+	ADSB_OPTION_OUTPUT_FORMAT_AVR = 'c',
+	/** Output Format: Binary */
+	ADSB_OPTION_OUTPUT_FORMAT_BIN = 'C',
+
+	/** Filter: output DF-11/17/18 frames only */
+	ADSB_OPTION_FRAME_FILTER_DF_11_17_18_ONLY = 'D',
+	/** Filter: output all frames */
+	ADSB_OPTION_FRAME_FILTER_ALL = 'd',
+
+	/** MLAT Information: include MLAT when using AVR Output format */
+	ADSB_OPTION_AVR_FORMAT_MLAT = 'E',
+	/** MLAT Information: don't include MLAT when using AVR Output format */
+	ADSB_OPTION_AVR_FORMAT_NO_MLAT = 'e',
+
+	/** CRC: check DF-11/17/18 frames */
+	ADSB_OPTION_DF_11_17_18_CRC_ENABLED ='f',
+	/** CRC: don't check DF-11/17/18 frames */
+	ADSB_OPTION_DF_11_17_18_CRC_DISABLED ='F',
+
+	/** Timestamp Source: GPS */
+	ADSB_OPTION_TIMESTAMP_SOURCE_GPS = 'G',
+	/** Timestamp Source: Legacy 12 MHz Clock */
+	ADSB_OPTION_TIMESTAMP_SOURCE_LEGACY_12_MHZ = 'g',
+
+	/** RTS Handshake: enabled */
+	ADSB_OPTION_RTS_HANDSHAKE_ENABLED = 'H',
+	/** RTS Handshake: disabled */
+	ADSB_OPTION_RTS_HANDSHAKE_DISABLED = 'h',
+
+	/** FEC: enable error correction on DF-11/17/18 frames */
+	ADSB_OPTION_DF_17_18_FEC_ENABLED = 'i',
+	/** FEC: disable error correction on DF-11/17/18 frames */
+	ADSB_OPTION_DF_17_18_FEC_DISABLED = 'I',
+
+	/** Mode-AC messages: enable decoding */
+	ADSB_OPTION_MODE_AC_DECODING_ENABLED = 'J',
+	/** Mode-AC messages: disable decoding */
+	ADSB_OPTION_MODE_AC_DECODING_DISABLED = 'j'
+};
+
 #endif
