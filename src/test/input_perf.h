b503d99e529d9b6940bc1ce652691f5053953b33
Markus Engel <m_engel@cs.uni-kl.de>
2015-09-05 22:39:22 +0200

    performance test for adsb receiver

---
+++ src/test/input_perf.h
@@ -0,0 +1,14 @@
+#ifndef _HAVE_INPUT_TEST_H
+#define _HAVE_INPUT_TEST_H
+
+#include <adsb.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+size_t INPUT_buildFrame(uint8_t * buf, enum ADSB_FRAME_TYPE type,
+	uint64_t mlat, int8_t siglevel, const uint8_t * payload, size_t payloadLen);
+
+void INPUT_setBuffer(uint8_t * buf, size_t length);
+
+#endif
