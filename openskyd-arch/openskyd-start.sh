d5cd410567d0d45e56483a6163151da50888ef8a
Markus Engel <m_engel@cs.uni-kl.de>
2015-09-21 18:42:11 +0200

    archlinux: changed package contents

--- openskyd-arch/openskyd-start.sh
+++ openskyd-arch/openskyd-start.sh
@@ -1,40 +1,44 @@
 #!/bin/sh
 
-BOARD=$(dmesg | grep compatible-baseboard | cut -d',' -f2)
-SLOTS=$(find /sys/devices -name slots)
+KERNEL=$(uname -r)
+KMAJOR=${KERNEL%%.*}
 
-BBUART5STATE=`grep BB-UART5 $SLOTS | wc -l`
-BBUART2STATE=`grep BB-UART2 $SLOTS | wc -l`
-BBRadarcapeSTATE=`grep BB-.-Radarcape $SLOTS | wc -l`
-
-if [ $BBUART5STATE -eq 0 ]; then 
-  echo BB-UART5 > ${SLOTS}
-fi
-
-if [ $BBUART2STATE == 0 ]; then
-  echo BB-UART2 > ${SLOTS}
-fi
-
-if [ $BBRadarcapeSTATE == 0 ]; then
-  if [ $BOARD == beaglebone ]; then
-    echo BB-W-Radarcape > ${SLOTS}
-
-  elif [ $BOARD == beaglebone-black ]; then
-    echo BB-B-Radarcape > ${SLOTS}
-
-  else
-    echo "Non supported board $BOARD"
+if [ ${KMAJOR} -eq 4 ]; then
+  COMPAT=/sys/firmware/devicetree/base/compatible
+  if grep "bone-black" ${COMPAT} >/dev/null 2>&1; then
+    PARAM="--black"
+  elif grep -v "bone" ${COMPAT} >/dev/null 2>&1; then
+    echo "Unkown base board"
     exit 1
+  else
+    PARAM=""
   fi
-  
-  cat $SLOTS
-fi
-
-if [ $BOARD == beaglebone-black ]; then
-  arg=--black
 else
-  arg=
+  BOARD=$(dmesg | grep compatible-baseboard | cut -d',' -f2)
+  case ${BOARD} in
+    beaglebone-black) OVERLAY=B; PARAM="--black";;
+    beaglebone) OVERLAY=W; PARAM="";;
+    *) echo "Non supported board ${BOARD}"
+       exit 1;;
+  esac
+
+  SLOTS=$(find /sys/devices -name slots)
+  UART2=$(grep "BB-UART2" ${SLOTS} | wc -l)
+  UART5=$(grep "BB-UART5" ${SLOTS} | wc -l)
+  CAPE=$(grep "BB-${OVERLAY}-Radarcape" ${SLOTS} | wc -l)
+
+  if [ ${UART2} -eq 0 ]; then
+    echo BB-UART2 > ${SLOTS}
+  fi
+
+  if [ ${UART5} -eq 0 ]; then
+    echo BB-UART5 > ${SLOTS}
+  fi
+
+  if [ ${CAPE} -eq 0 ]; then
+    echo BB-${OVERLAY}-Radarcape > ${SLOTS}
+  fi
 fi
 
-exec /usr/bin/openskyd $arg
+exec /usr/bin/openskyd ${PARAM}
 
