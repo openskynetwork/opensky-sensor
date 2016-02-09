81d83a28cd1b697adfc7ead7574dc575d239785a
Markus Engel <engel@sero-systems.de>
2016-02-09 18:19:48 +0100

    added opensky bridge for library

---
+++ src/opensky.c
@@ -0,0 +1,68 @@
+/* Copyright (c) 2015-2016 OpenSky Network <engel at opensky-network dot org> */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#include "opensky.h"
+#include <buffer.h>
+#include <component.h>
+#include <network.h>
+#include <relay.h>
+
+#include <assert.h>
+#include <string.h>
+
+void OPENSKY_configure()
+{
+
+}
+
+void OPENSKY_start()
+{
+	COMP_register(&BUF_comp, NULL);
+	COMP_register(&NET_comp, NULL);
+	COMP_register(&RELAY_comp, NULL);
+
+	COMP_initAll();
+	COMP_startAll();
+}
+
+void OPENSKY_stop()
+{
+	COMP_stopAll();
+	COMP_destructAll();
+}
+
+static inline void append(uint8_t ** buf, uint8_t c)
+{
+	if ((*(*buf)++ = c) == 0x1a)
+		*(*buf)++ = 0x1a;
+}
+
+static inline void encode(uint8_t ** buf, const uint8_t * src, size_t len)
+{
+	while (len--)
+		append(buf, *src++);
+}
+
+void OPENSKY_frame(const struct OPENSKY_Frame * frame)
+{
+	struct ADSB_Frame * out = BUF_newFrame();
+	assert(out);
+	memcpy(out, frame, sizeof * frame);
+
+	uint8_t * buf = out->raw;
+	buf[0] = '\x1a';
+	buf[1] = frame->frameType + '1';
+
+	uint8_t * ptr = buf + 2;
+	uint64_t mlat = htobe64(frame->mlat);
+	encode(&ptr, ((uint8_t*)&mlat) + 2, 6);
+
+	append(&ptr, frame->siglevel);
+	encode(&ptr, (const uint8_t*)frame->payload, frame->payloadLen);
+
+	out->raw_len = ptr - buf;
+
+	BUF_commitFrame(out);
+}
---
+++ src/opensky.h
@@ -0,0 +1,41 @@
+/* Copyright (c) 2015-2016 OpenSky Network <engel at opensky-network dot org> */
+
+#ifndef _HAVE_OPENSKY_H
+#define _HAVE_OPENSKY_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum OPENSKY_FRAME_TYPE {
+	OPENSKY_FRAME_TYPE_MODE_AC = 0,
+	OPENSKY_FRAME_TYPE_MODE_S_SHORT = 1,
+	OPENSKY_FRAME_TYPE_MODE_S_LONG = 2,
+	OPENSKY_FRAME_TYPE_STATUS = 3,
+};
+
+struct OPENSKY_Frame {
+	enum OPENSKY_FRAME_TYPE frameType;
+
+	uint64_t mlat;
+	int8_t siglevel;
+
+	size_t payloadLen;
+	uint8_t payload[14];
+};
+
+void OPENSKY_configure();
+
+void OPENSKY_start();
+void OPENSKY_stop();
+
+void OPENSKY_frame(const struct OPENSKY_Frame * frame);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
