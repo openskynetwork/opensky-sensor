703fb5cef30818d916f232d45bdbcc6f1e01779a
Markus Engel <engel@sero-systems.de>
2016-02-10 22:42:07 +0100

    opensky glue: fixes, added defaults for configuration

--- src/opensky.c
+++ src/opensky.c
@@ -12,29 +12,71 @@
 #include <assert.h>
 #include <string.h>
 #include <util.h>
+#include <cfgfile.h>
+
+struct CFG_Config CFG_config;
+
+static bool configured;
+static bool running = false;
 
 __attribute__((visibility("default")))
 void OPENSKY_configure()
 {
+	strncpy(CFG_config.net.host, "collector.opensky-network.org",
+		sizeof CFG_config.net.host);
+	CFG_config.net.port = 10004;
+	CFG_config.net.reconnectInterval = 10;
+	CFG_config.net.timeout = 1500;
 
+	CFG_config.recv.modeSLongExtSquitter = true;
+	CFG_config.recv.syncFilter = true;
+
+	CFG_config.buf.history = false;
+	CFG_config.buf.statBacklog = 200;
+	CFG_config.buf.dynBacklog = 1000;
+	CFG_config.buf.dynIncrement = 1080;
+	CFG_config.buf.gcEnabled = false;
+	CFG_config.buf.gcInterval = 120;
+	CFG_config.buf.gcLevel = 2;
+
+	CFG_config.stats.enabled = false;
+
+	CFG_config.dev.serialSet = UTIL_getSerial(&CFG_config.dev.serial);
+
+	configured = true;
+
+	FILTER_init();
 }
 
 __attribute__((visibility("default")))
 void OPENSKY_start()
 {
-	COMP_register(&BUF_comp, NULL);
-	COMP_register(&NET_comp, NULL);
-	COMP_register(&RELAY_comp, NULL);
+	if (!configured)
+		OPENSKY_configure();
 
-	COMP_initAll();
-	COMP_startAll();
+	FILTER_reset();
+
+	if (configured) {
+		COMP_register(&BUF_comp, NULL);
+		COMP_register(&NET_comp, NULL);
+		COMP_register(&RELAY_comp, NULL);
+
+		COMP_initAll();
+		COMP_startAll();
+
+		running = true;
+	}
 }
 
 __attribute__((visibility("default")))
 void OPENSKY_stop()
 {
-	COMP_stopAll();
-	COMP_destructAll();
+	if (running) {
+		COMP_stopAll();
+		COMP_destructAll();
+
+		running = false;
+	}
 }
 
 static inline void append(uint8_t ** out, uint8_t c)
@@ -80,11 +122,14 @@ static inline void encode(uint8_t ** out, const uint8_t * in, size_t len)
 __attribute__((visibility("default")))
 void OPENSKY_frame(const struct OPENSKY_Frame * frame)
 {
-	struct ADSB_Frame * out;
-	if (unlikely(frame->payloadLen > ((sizeof out->raw) >> 1)))
+	if (unlikely(!configured))
 		return;
 
-	if (FILTER_filter((const struct ADSB_Frame*)frame))
+	struct ADSB_Frame * out;
+	if (unlikely(frame->payloadLen > ((sizeof out->raw) >> 1) - 9))
+		return;
+
+	if (!FILTER_filter((const struct ADSB_Frame*)frame))
 		return;
 
 	out = BUF_newFrame();
