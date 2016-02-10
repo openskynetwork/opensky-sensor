7c683cd5491d2921093492dc2eda6b65c2d82cfa
Markus Engel <engel@sero-systems.de>
2016-02-10 22:42:25 +0100

    opensky: added example for library

---
+++ src/opensky-example.c
@@ -0,0 +1,39 @@
+
+#include <opensky.h>
+#include <unistd.h>
+
+int main()
+{
+	OPENSKY_configure();
+
+	OPENSKY_start();
+
+	/* sleep is normally not needed, just for some testing purpose */
+	sleep(1);
+
+	const struct OPENSKY_Frame frameSync = {
+		.frameType = OPENSKY_FRAME_TYPE_STATUS,
+		.mlat = 1
+	};
+
+	OPENSKY_frame(&frameSync);
+
+	struct OPENSKY_Frame frame = {
+		.frameType = OPENSKY_FRAME_TYPE_MODE_S_LONG,
+		.siglevel = 100,
+		.mlat = 123456789012345,
+		.payload = { 0x8a },
+		.payloadLen = 14
+	};
+
+	uint32_t i = 0;
+	for (i = 0; i < 10; ++i) {
+		frame.payload[1] = i;
+		OPENSKY_frame(&frame);
+	}
+
+	/* sleep is normally not needed, just for some testing purpose */
+	sleep(1);
+
+	OPENSKY_stop();
+}
