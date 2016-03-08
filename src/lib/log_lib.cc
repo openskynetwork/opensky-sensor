1d0b8df071b26bc981b3b30c982adc9f66eeb792
Markus Engel <engel@sero-systems.de>
2016-03-08 12:56:05 +0100

    log lib: provided implementation for logging with errno

--- src/lib/log_lib.cc
+++ src/lib/log_lib.cc
@@ -7,6 +7,8 @@
 #include <threads.h>
 #include <util.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 struct LevelName {
 	const char name[6];
@@ -41,8 +43,10 @@ void LOG_log(enum LOG_LEVEL level, const char * prefix, const char * str)
 	std::cout << ' ' << str << std::endl;
 	CANCEL_RESTORE(&r);
 
-	if (unlikely(level == LOG_LEVEL_ERROR))
+	if (unlikely(level == LOG_LEVEL_ERROR)) {
+		LOG_flush();
 		exit(EXIT_FAILURE);
+	}
 }
 
 void LOG_flush()
@@ -50,18 +54,61 @@ void LOG_flush()
 	std::flush(std::cout);
 }
 
+static void logWithErr(enum LOG_LEVEL level, int err, const char * prefix,
+	const char * fmt, va_list ap)
+{
+	char str[2048];
+	vsnprintf(str, sizeof str, fmt, ap);
+
+	char errbuf[100];
+	char * errstr;
+#if (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! _GNU_SOURCE
+	int rc = strerror_r(err, errbuf, sizeof errbuf);
+	if (rc == 0)
+		errstr = errbuf;
+	else
+		errstr = NULL;
+#else
+	errstr = strerror_r(err, errbuf, sizeof errbuf);
+#endif
+
+	int r;
+	CANCEL_DISABLE(&r);
+
+	std::cout << '[' << levelNames[level].name << ']';
+	if (prefix)
+		std::cout << ' ' << '[' << prefix << ']';
+	std::cout << ' ' << str;
+
+	if (errstr)
+		std::cout << ':' << errstr << ' ' << '(' << err << ')' << std::endl;
+	else
+		std::cout << ": Unknown error (" << err << ')' << std::endl;
+
+	CANCEL_RESTORE(&r);
+
+	if (unlikely(level == LOG_LEVEL_ERROR)) {
+		LOG_flush();
+		exit(EXIT_FAILURE);
+	}
+}
+
 __attribute__((format(printf, 3, 4)))
 void LOG_errno(enum LOG_LEVEL level, const char * prefix, const char * fmt,
 	...)
 {
-	if (unlikely(level == LOG_LEVEL_ERROR))
-		exit(EXIT_FAILURE);
+	va_list ap;
+	va_start(ap, fmt);
+	logWithErr(level, errno, prefix, fmt, ap);
+	va_end(ap);
 }
 
 __attribute__((format(printf, 4, 5)))
 void LOG_errno2(enum LOG_LEVEL level, int err, const char * prefix,
 	const char * fmt, ...)
 {
-	if (unlikely(level == LOG_LEVEL_ERROR))
-		exit(EXIT_FAILURE);
+	va_list ap;
+	va_start(ap, fmt);
+	logWithErr(level, err, prefix, fmt, ap);
+	va_end(ap);
 }
