e1e58b6867ab7e5f314290cb66c0b1827246157d
Markus Engel <engel@opensky-network.org>
2016-05-22 19:34:57 +0200

    gps: send position to server if needed

--- src/gps.c
+++ src/gps.c
@@ -5,10 +5,13 @@
 #include <errno.h>
 #include <time.h>
 #include <pthread.h>
+#include <threads.h>
 #include <inttypes.h>
 #include <gps_parser.h>
+#include <network.h>
+#include <stdio.h>
 
-static void timeFrame(const uint8_t * buf);
+//static void timeFrame(const uint8_t * buf);
 static void posFrame(const uint8_t * buf);
 
 static void construct();
@@ -22,9 +25,128 @@ struct Component GPS_comp = {
 	.main = &mainloop
 };
 
-static const time_t gpsEpoch = 315964800;
+/** Receiver Mode */
+enum GPS_RECV_MODE {
+	/** Automatic (2d/3d) */
+	GPS_RECV_MODE_AUTOMATIC = 0,
+	/** Single Satellite (Time) */
+	GPS_RECV_MODE_SNGL_SAT = 1,
+	/** Horizontal (2d) */
+	GPS_RECV_MODE_HORIZONTAL = 3,
+	/** Full Position (3d) */
+	GPS_RECV_MODE_FULL_POS = 4,
+	/** Over-Determined Clock */
+	GPS_RECV_MODE_ODT_CLK = 7,
+};
 
-_Atomic enum GPS_TIME_FLAGS GPS_timeFlags = ATOMIC_VAR_INIT(GPS_TIME_FLAG_NONE);
+/** Disciplining Mode */
+enum GPS_DISC_MODE {
+	/** Normal (Locked to GPS) */
+	GPS_DISC_MODE_LOCKED = 0,
+	/** Power Up */
+	GPS_DISC_MODE_PWR_UP = 1,
+	/** Auto Holdover */
+	GPS_DISC_MODE_AUTO_HOLDOVER = 2,
+	/** Manual Holdover */
+	GPS_DISC_MODE_MANUAL_HOLDOVER = 3,
+	/** Recovery */
+	GPS_DISC_MODE_RECOVERY = 4,
+	/** Disciplining disabled */
+	GPS_DISC_MODE_DISC_DISABLED = 6
+};
+
+/** Critical Alarms */
+enum GPS_CRIT_ALARM {
+	/** Oscillator Control Voltage is at rail */
+	GPS_CRIT_ALARM_DAC_AT_RAIL = 1 << 4
+};
+
+/** Minor Alarms */
+enum GPS_MIN_ALARM {
+	/** Oscillator Control Voltage is near a rail. Oscillator is within
+	 * 2 years of becoming untunable.
+	 */
+	GPS_MIN_ALARM_DAC_NEAR_RAIL = 1 << 0,
+	/** Antenna input is not drawing sufficient current, most likely
+	 * unconnected
+	 */
+	GPS_MIN_ALARM_ANT_OPEN = 1 << 1,
+	/** Antenna input is drawing too much current, most likely broken cable or
+	 * antenna
+	 */
+	GPS_MIN_ALARM_ANT_SHORT = 1 << 2,
+	/** No satellites are usable */
+	GPS_MIN_ALARM_NOT_TRACKING_SATS = 1 << 3,
+	GPS_MIN_ALARM_NOT_DISC_OSC = 1 << 4,
+	/** Self-Survey procedure is in progress */
+	GPS_MIN_ALARM_SURVEY_IN_PROGRESS = 1 << 5,
+	/** No accurate position stored in the flash ROM */
+	GPS_MIN_ALARM_NO_STORED_POSITION = 1 << 6,
+	/** Leap second transition is pending */
+	GPS_MIN_ALARM_LEAP_SECOND_PEND = 1 << 7,
+	/** the receive is operating in one of its test modes */
+	GPS_MIN_ALARM_IN_TEST_MODE = 1 << 8,
+	/** Accuracy of the position used for time only fixes is questionable */
+	GPS_MIN_ALARM_POSITION_QUESTIONABLE = 1 << 9,
+	GPS_MIN_ALARM_ALMANAC_INCOMPLETE = 1 << 11,
+	GPS_MIN_ALARM_PPS_NOT_GENERATED = 1 << 12
+};
+
+/** GPS Decoding Status */
+enum GPS_DECODE_STATUS {
+	/** Doing fixes */
+	GPS_DECODE_STATUS_DOING_FIXES = 0x00,
+	/** Don't have GPS time */
+	GPS_DECODE_STATUS_NO_GPS_TIME = 0x01,
+	/** PDOP too high */
+	GPS_DECODE_STATUS_PDOP_TOO_HIGH = 0x03,
+	/** No usable sats */
+	GPS_DECODE_STATUS_NO_SATS = 0x08,
+	/** Only 1 usable sat */
+	GPS_DECODE_STATUS_1SAT = 0x09,
+	/** Only 2 usable sats */
+	GPS_DECODE_STATUS_2SAT = 0x0a,
+	/** Only 3 usable sats */
+	GPS_DECODE_STATUS_3SAT = 0x0b,
+	/** The chosen sat is unusable */
+	GPS_DECODE_STATUS_UNUSABLE = 0x0c,
+	/** TRAIM rejected the fix */
+	GPS_DECODE_STATUS_TRAIM_REJECT = 0x10
+};
+
+/** Disciplining Activity */
+enum GPS_DISC_ACTIVITY {
+	/** Phase locking */
+	GPS_DISC_ACTIVITY_PHASE_LOCKING = 0,
+	/** Oscillator warm-up */
+	GPS_DISC_ACTIVITY_OSC_WARMUP = 1,
+	/** Frequency locking */
+	GPS_DISC_ACTIVITY_FREQ_LOCK = 2,
+	/** Placing PPS */
+	GPS_DISC_ACTIVITY_PLACING_PPS = 3,
+	/** Initializing loop filter */
+	GPS_DISC_ACTIVITY_INIT_LOOP_FILTER = 4,
+	/** Compensating OCXO (holdover) */
+	GPS_DISC_ACTIVITY_COMP_OCXO = 5,
+	/** Inactive */
+	GPS_DISC_ACTIVITY_INACTIVE = 6,
+	/** Recovery mode */
+	GPS_DISC_ACTIVITY_RECOVERY = 8,
+	/** Calibration/control voltage */
+	GPS_DISC_ACTIVITY_CALIBRATION = 9
+};
+
+//static const time_t gpsEpoch = 315964800;
+
+#define R2D 57.2957795130823208767981548141051703
+
+//_Atomic enum GPS_TIME_FLAGS GPS_timeFlags = ATOMIC_VAR_INIT(GPS_TIME_FLAG_NONE);
+
+static pthread_mutex_t posMutex = PTHREAD_MUTEX_INITIALIZER;
+static struct GPS_RawPosition position;
+static bool hasPosition;
+
+static bool needPosition;
 
 static void construct()
 {
@@ -38,39 +160,49 @@ static void destruct()
 
 static void mainloop()
 {
+	needPosition = false;
+
 	while (true) {
-		uint8_t buf[1024];
-		size_t len = GPS_PARSER_getFrame(buf, sizeof buf);
-		if (!len)
-			continue;
-		switch (buf[0]) {
-		case 0x8f:
-			if (len < 2)
+		GPS_PARSER_connect();
+		hasPosition = false;
+
+		printf("GPS: connected\n");
+
+		while (true) {
+			uint8_t buf[1024];
+			size_t len = GPS_PARSER_getFrame(buf, sizeof buf);
+			if (!len)
 				continue;
-			switch (buf[1]) {
-			case 0xab:
-				if (len == 18)
-					timeFrame(buf + 1);
+			switch (buf[0]) {
+			case 0x8f:
+				if (len < 2)
+					continue;
+				switch (buf[1]) {
+				/*case 0xab:
+					if (len == 18)
+						timeFrame(buf + 1);
+					break;*/
+				case 0xac:
+					if (len == 69)
+						posFrame(buf + 1);
+					break;
+				/*case 0xa2:
+					if (len == 3)
+						ioFrame2(buf + 1);*/
+				}
 				break;
-			case 0xac:
-				if (len == 69)
-					posFrame(buf + 1);
+			case 0x46:
+				//statFrame(buf);
 				break;
-			/*case 0xa2:
-				if (len == 3)
-					ioFrame2(buf + 1);*/
+			case 0x55:
+				//ioFrame(buf);
+				break;
 			}
-			break;
-		case 0x46:
-			//statFrame(buf);
-			break;
-		case 0x55:
-			//ioFrame(buf);
-			break;
 		}
 	}
 }
 
+#if 0
 static void timeFrame(const uint8_t * buf)
 {
 	uint_fast32_t secondsOfWeek = GPS_tou32(buf + 1);
@@ -118,17 +250,65 @@ static void timeFrame(const uint8_t * buf)
 	}*/
 #endif
 }
-
-#define R2D 57.2957795130823208767981548141051703
+#endif
 
 static void posFrame(const uint8_t * buf)
 {
+	enum GPS_RECV_MODE recvMode = buf[1];
+	enum GPS_DISC_MODE discMode = buf[2];
+	//uint8_t selfSurveyProgress = buf[3];
+	//uint32_t holdOverDuration = GPS_tou32(buf + 4);
+	uint16_t critAlarms = GPS_tou16(buf + 8);
+	uint16_t minorAlarms = GPS_tou16(buf + 10);
+	enum GPS_DECODE_STATUS decodingStatus = buf[12];
+	//enum GPS_DISC_ACTIVITY discActivity = buf[13];
+	/*float ppsOffset = GPS_tofloat(buf + 16);
+	float clockOffset = GPS_tofloat(buf + 20);
+	uint32_t dacValue = GPS_tou32(buf + 24);
+	float dacVoltage = GPS_tofloat(buf + 28);
+	float temperature = GPS_tofloat(buf+ 32);
 	double latitude = GPS_todouble(buf + 36) * R2D;
 	double longitude = GPS_todouble(buf + 44) * R2D;
-	//double alt = todouble(buf + 52);
-	uint16_t alarms = GPS_tou16(buf + 10);
-	bool antError = !!(alarms & 3);
-	bool fixes = buf[12] == 0;
+	double altitude = todouble(buf + 52);
+	float ppsQuantizationError = tofloat(buf + 60);*/
+
+	bool selfSurvey = !!(minorAlarms & GPS_MIN_ALARM_SURVEY_IN_PROGRESS);
+	bool inTestMode = !!(minorAlarms & GPS_MIN_ALARM_IN_TEST_MODE);
+
+	pthread_mutex_lock(&posMutex);
+	if (critAlarms || inTestMode || recvMode != GPS_RECV_MODE_ODT_CLK ||
+		discMode != GPS_DISC_MODE_LOCKED) {
+		/* unrecoverable or not automatically solvable by now */
+		NOC_puts("GPS: unsolved");
+		pthread_mutex_unlock(&posMutex);
+		return;
+	}
+
+	if (selfSurvey || decodingStatus != GPS_DECODE_STATUS_DOING_FIXES ||
+		discMode != GPS_DISC_MODE_LOCKED) {
+		/* no position -> wait */
+		NOC_puts("GPS: No pos\n");
+		pthread_mutex_unlock(&posMutex);
+		return;
+	}
+
+	memcpy(&position, buf + 36, sizeof(uint64_t) * 3);
+	if (!hasPosition) {
+		double latitude = GPS_todouble(buf + 36) * R2D;
+		double longitude = GPS_todouble(buf + 44) * R2D;
+		double altitude = GPS_todouble(buf + 52);
+		NOC_printf("GPS: Got LLA: %+6.2f %+6.2f %+6.2f\n", latitude, longitude,
+			altitude);
+	}
+	hasPosition = true;
+	pthread_mutex_unlock(&posMutex);
+
+	if (hasPosition && needPosition) {
+		NOC_puts("GPS: Sending Position to server");
+		needPosition = !NET_sendPosition(&position);
+		if (needPosition)
+			NOC_puts("GPS: Could not send position to server, retrying");
+	}
 
 #if 0
 	pthread_mutex_lock(&GPS_gps.mutex);
@@ -145,4 +325,19 @@ static void posFrame(const uint8_t * buf)
 #endif
 }
 
+bool GPS_getRawPosition(struct GPS_RawPosition * rawPos)
+{
+	pthread_mutex_lock(&posMutex);
+	if (!hasPosition) {
+		pthread_mutex_unlock(&posMutex);
+		return false;
+	}
+	memcpy(rawPos, &position, sizeof *rawPos);
+	pthread_mutex_unlock(&posMutex);
+	return true;
+}
 
+void GPS_setNeedPosition()
+{
+	needPosition = true;
+}
