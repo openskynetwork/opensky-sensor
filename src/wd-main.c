9feabd404be26c29a20fafc5183612add87a575d
Markus Engel <engel@opensky-network.org>
2016-09-14 12:23:32 +0200

    wd: removed options for fpga configuration

--- src/wd-main.c
+++ src/wd-main.c
@@ -4,23 +4,13 @@
 #include <config.h>
 #endif
 #include <stdlib.h>
-#include <unistd.h>
 #include <getopt.h>
-#include <string.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include "gpio.h"
 #include "watchdog.h"
-#include "cfgfile.h"
 #include "util.h"
-#include "log.h"
-
-#if defined(LOCAL_FILES) && !defined(SYSCONFDIR)
-#define SYSCONFDIR "."
-#endif
 
 static struct option opts[] = {
-	{ .name = "black", .has_arg = no_argument, .val = 'b' },
 	{ .name = "help", .has_arg = no_argument, .val = 'h' },
 	{}
 };
@@ -28,16 +18,11 @@ static struct option opts[] = {
 int main(int argc, char * argv[])
 {
 	int opt;
-	bool bbwhite = true;
 
 	while ((opt = getopt_long(argc, argv, "h", opts, NULL)) != -1) {
 		switch (opt) {
-		case 'b':
-			bbwhite = false;
-			break;
 		default:
-			fprintf(stderr,
-				"Usage: %s [--black] [--help|-h]\n", argv[0]);
+			fprintf(stderr, "Usage: %s [--help|-h]\n", argv[0]);
 			return EXIT_FAILURE;
 		}
 	}
