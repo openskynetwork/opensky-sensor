948a851cb852bca6c09c2b22212a8e84db6761f6
Markus Engel <engel@opensky-network.org>
2016-11-26 01:50:56 +0100

    dump1090: added logging, fixed some issues, ensure configuration directory

--- src/dump1090/dump1090.c
+++ src/dump1090/dump1090.c
@@ -9,10 +9,14 @@
 #include <pthread.h>
 #include <string.h>
 #include <error.h>
+#include <errno.h>
 #include <sys/time.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <getopt.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include "core/buffer.h"
 #include "core/network.h"
 #include "core/tb.h"
@@ -21,22 +25,60 @@
 #include "core/gps.h"
 #include "core/login.h"
 #include "core/serial.h"
+#include "core/beast.h"
 #include "util/cfgfile.h"
 #include "util/statistics.h"
 #include "util/log.h"
 #include "util/util.h"
+#include "req-serial.h"
 
 static const char PFX[] = "MAIN";
 
 #if (defined(ECLIPSE) || defined(LOCAL_FILES)) && !defined(SYSCONFDIR)
 #define SYSCONFDIR "."
 #endif
+#if (defined(ECLIPSE) || defined(LOCAL_FILES)) && !defined(LOCALSTATEDIR)
+#define LOCALSTATEDIR "var"
+#endif
 
 static void sigint(int sig);
 
-static bool run;
-static pthread_mutex_t sigmutex = PTHREAD_MUTEX_INITIALIZER;
-static pthread_cond_t sigcond = PTHREAD_COND_INITIALIZER;
+static char username[BEAST_MAX_USERNAME + 1];
+
+static struct CFG_Section cfg =
+{
+	.name = "Ident",
+	.n_opt = 1,
+	.options = {
+		{
+			.name = "user",
+			.type = CFG_VALUE_TYPE_STRING,
+			.var = &username,
+			.maxlen = BEAST_MAX_USERNAME,
+		}
+	}
+};
+
+static void ensureDir(const char * path)
+{
+	struct stat st;
+	int rc = stat(path, &st);
+	if (rc == -1) {
+		if (errno == ENOENT) {
+			rc = mkdir(path, 0755);
+			if (rc == -1) {
+				LOG_errno(LOG_LEVEL_EMERG, PFX,
+					"Could not create configuration directory '%s'", path);
+			}
+		} else if (errno == EACCES) {
+			LOG_errno(LOG_LEVEL_EMERG, PFX,
+				"Could not access configuration directory '%s'", path);
+		}
+	} else if (!S_ISDIR(st.st_mode)) {
+		LOG_errno(LOG_LEVEL_EMERG, PFX,
+			"Configuration path '%s' is not a directory", path);
+	}
+}
 
 int main(int argc, char * argv[])
 {
@@ -49,20 +91,30 @@ int main(int argc, char * argv[])
 	gettimeofday(&tv, NULL);
 	srand(tv.tv_sec + tv.tv_usec);
 
-	LOGIN_setDeviceID(LOGIN_DEVICE_ID_FEEDER);
+	LOGIN_setDeviceType(LOGIN_DEVICE_TYPE_FEEDER);
+	LOGIN_setUsername(username);
 
 	COMP_register(&TB_comp);
 	COMP_register(&RELAY_comp);
 	COMP_register(&RECV_comp);
 	COMP_register(&STAT_comp);
+	COMP_register(&SERIAL_comp);
 
 	COMP_fixup();
 
+	CFG_registerSection(&cfg);
+
+	ensureDir(LOCALSTATEDIR);
+	ensureDir(LOCALSTATEDIR "/conf.d");
+
 	/* read & check configuration */
 	CFG_loadDefaults();
-	bool cfg = CFG_readDirectory("/var/lib/openskyd/conf.d", true, true, true, false);
-	cfg = cfg && CFG_readDirectory("/etc/openskyd/conf.d", true, true, true, false);
-	if (!cfg)
+	// TODO: test, test, test
+	bool cfgVar = CFG_readDirectory(LOCALSTATEDIR "/conf.d", true, true, true,
+		false);
+	bool cfgEtc = CFG_readDirectory(SYSCONFDIR "/conf.d", true, true, true,
+		false);
+	if (!cfgEtc || !cfgVar)
 		LOG_log(LOG_LEVEL_WARN, PFX,
 			"Could not read all configuration files");
 
@@ -81,15 +133,11 @@ int main(int argc, char * argv[])
 			"quitting");
 	}
 
-	run = true;
-	pthread_mutex_lock(&sigmutex);
 #ifdef CLEANUP_ROUTINES
 	signal(SIGINT, &sigint);
 #endif
-	while (run)
-		pthread_cond_wait(&sigcond, &sigmutex);
+	pause();
 	signal(SIGINT, SIG_DFL);
-	pthread_mutex_unlock(&sigmutex);
 
 	COMP_stopAll();
 	COMP_destructAll();
@@ -103,9 +151,5 @@ int main(int argc, char * argv[])
 #ifdef CLEANUP_ROUTINES
 static void sigint(int sig)
 {
-	pthread_mutex_lock(&sigmutex);
-	run = false;
-	pthread_cond_broadcast(&sigcond);
-	pthread_mutex_unlock(&sigmutex);
 }
 #endif
