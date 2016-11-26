270e9f75a13cc4432d742501fbabc0a20ec0fb9b
Markus Engel <engel@opensky-network.org>
2016-11-26 02:07:31 +0100

    lib test: fixed serial number dummy

--- src/test/glue.c
+++ src/test/glue.c
@@ -1,8 +1,8 @@
 /* Copyright (c) 2015-2016 OpenSky Network <contact@opensky-network.org> */
 
-#include "util/util.h"
+#include "util/serial_eth.h"
 
-bool UTIL_getSerial(uint32_t * serial)
+bool SERIAL_ETH_getSerial(uint32_t * serial)
 {
 	if (serial)
 		*serial = 0xdeadbeef;
