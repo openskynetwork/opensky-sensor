5e4da423a5e33b84bd9b63281897aaeb48e87656
Markus Engel <engel@opensky-network.org>
2016-12-26 15:20:48 +0100

    beast: comments, cleanups

--- src/core/beast.c
+++ src/core/beast.c
@@ -10,10 +10,11 @@
 
 /** Escape all SYNC characters in a packet to comply with the beast frame
  *   format.
- * \param out output buffer, must be large enough (twice the length)
- * \param in input buffer
- * \param len input buffer length
- * \note input and output buffers may not overlap
+ * @param out output buffer, must be large enough (twice the length)
+ * @param in input buffer
+ * @param len input buffer length
+ * @return used output buffer length
+ * @note input and output buffers may not overlap
  */
 size_t BEAST_encode(uint8_t * __restrict out, const uint8_t * __restrict in,
 	size_t len)
@@ -21,28 +22,40 @@ size_t BEAST_encode(uint8_t * __restrict out, const uint8_t * __restrict in,
 	if (unlikely(!len))
 		return 0;
 
+	/* general idea is to search for a SYNC, copy the buffer up to (including)
+	 * the sync and then point to SYNC and search for the next SYNC.
+	 * In that second copy, we copy the first SYNC _again_ to the output buffer,
+	 * thereby doubling it.
+	 * Besides dealing with a regular end of buffer, we also have to deal with
+	 * special cases such as the beginning, the last SYNC or if there is no
+	 * SYNC at all.
+	 */
+
 	uint8_t * ptr = out;
 	const uint8_t * end = in + len;
 
-	/* first time: search for escape from in up to len */
-	const uint8_t * esc = (uint8_t*) memchr(in, BEAST_SYNC, len);
+	/* first time: search for SYNC from input buffer up to len */
+	const uint8_t * sync = (uint8_t*)memchr(in, BEAST_SYNC, len);
 	while (true) {
-		if (unlikely(esc)) {
-			/* if esc found: copy up to (including) esc */
-			memcpy(ptr, in, esc + 1 - in);
-			ptr += esc + 1 - in;
-			in = esc; /* important: in points to the esc now */
+		if (unlikely(sync)) {
+			/* if SYNC found: copy up to (including) SYNC */
+			memcpy(ptr, in, sync + 1 - in);
+			ptr += sync + 1 - in;
+			in = sync; /* important: in points to that SYNC now */
 		} else {
-			/* no esc found: copy rest, fast return */
+			/* no (more) SYNC found: copy rest, fast return */
 			memcpy(ptr, in, end - in);
 			return ptr + (end - in) - out;
 		}
 		if (likely(end != in + 1)) {
-			esc = (uint8_t*) memchr(in + 1, BEAST_SYNC, end - in - 1);
+			/* there is some input left -> search for next SYNC, this time
+			 * beginning _after_ the current SYNC
+			 */
+			sync = (uint8_t*)memchr(in + 1, BEAST_SYNC, end - in - 1);
 		} else {
-			/* nothing more to do, but the last BEAST_SYNC is still to be copied
-			 * instead of setting esc = NULL and re-iterating, we do things
-			 * faster here.
+			/* nothing more to do, but the last SYNC is still to be copied ->
+			 * instead of setting sync = NULL and re-iterating, we use a little
+			 * shortcut here.
 			 */
 			*ptr = BEAST_SYNC;
 			return ptr + 1 - out;
--- src/core/beast.h
+++ src/core/beast.h
@@ -11,33 +11,53 @@ extern "C" {
 #endif
 
 enum {
+	/** Constant for frame synchronization */
 	BEAST_SYNC = '\x1a'
 };
 
+/** Message identifiers for extended beast protocol */
 enum BEAST_TYPE {
+	/** Mode AC frame */
 	BEAST_TYPE_MODE_AC = '1',
+	/** Mode-S Short frame */
 	BEAST_TYPE_MODE_S_SHORT = '2',
+	/** Mode-S Long frame */
 	BEAST_TYPE_MODE_S_LONG = '3',
+	/** Status frame (sent by radarcape only) */
 	BEAST_TYPE_STATUS = '4',
 
+	/** Serial number */
 	BEAST_TYPE_SERIAL = '5',
+	/** Keep alive */
 	BEAST_TYPE_KEEP_ALIVE = '6',
+	/** GPS Position */
 	BEAST_TYPE_GPS_POSITION = '7',
 
+	/** Device ID */
 	BEAST_TYPE_DEVICE_ID = 65,
+	/** Serial number request */
 	BEAST_TYPE_SERIAL_REQ = 66,
+	/** User name */
 	BEAST_TYPE_USER = 67,
 };
 
+/** Device types for extended beast protocol */
 enum BEAST_DEVICE_TYPE {
+	/** Invalid (i.e. unconfigured) */
 	BEAST_DEVICE_TYPE_INVALID = 0,
+	/** Bogus (sending random frames) */
 	BEAST_DEVICE_TYPE_BOGUS = 1,
+	/** Standalone radarcape */
 	BEAST_DEVICE_TYPE_RADARCAPE = 2,
+	/** Radarcape via network (e.g. Angstrom/debian package)*/
 	BEAST_DEVICE_TYPE_RADARCAPE_NET = 3,
+	/** Radarcape via library */
 	BEAST_DEVICE_TYPE_RADARCAPE_LIB = 4,
+	/** Dump1090 Feeder */
 	BEAST_DEVICE_TYPE_FEEDER = 5,
 };
 
+/** Maximal length of username. MUST match the server side */
 #define BEAST_MAX_USERNAME 40
 
 size_t BEAST_encode(uint8_t * __restrict out, const uint8_t * __restrict in,
