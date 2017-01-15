a0563329755fc50d4a94d0d7568b58e5519eb583
Markus Engel <engel@opensky-network.org>
2017-01-15 23:18:27 +0100

    buffer: comments

--- src/core/buffer.c
+++ src/core/buffer.c
@@ -20,18 +20,20 @@
 #include "util/util.h"
 #include "util/list.h"
 
+/** Component: Prefix */
 static const char PFX[] = "BUF";
 
-/** Define to 1 to enable debugging messages */
+/* Define to 1 to enable debugging messages */
 //#define BUF_DEBUG 1
 
 /* Forward declaration */
 struct Pool;
 
-/** Linked Frame List */
+/** Container for an OpenSky frame */
 struct FrameLink {
 	/** Frame */
 	struct OPENSKY_RawFrame frame;
+	/** Linked list: siblings */
 	struct LIST_LinkD list;
 	/** Containing pool */
 	struct Pool * pool;
@@ -39,9 +41,11 @@ struct FrameLink {
 
 /** Buffer Pool */
 struct Pool {
+	/** Pool: vector of frame links */
 	struct FrameLink * links;
+	/** Number of frame links */
 	size_t size;
-	/** Linked List: next pool */
+	/** Linked List: siblings */
 	struct LIST_LinkD list;
 	/** Garbage collection: collected frames */
 	struct LIST_ListDC collect;
@@ -50,123 +54,56 @@ struct Pool {
 /** Static Pool (those frames are always allocated in advance) */
 static struct Pool staticPool;
 
-/** Dynamic Pool max. increments */
-static size_t dynMaxIncrements;
-/** Dynamic Pool List */
+/** Dynamic Pool: max number of dynamic pools */
+static size_t dynMaxPools;
+/** Dynamic Pool: list of dynamic pools */
 static struct LIST_ListDC dynPools;
 
-/** Overall Pool */
+/** Overall frame pool */
 static struct LIST_ListDC pool;
-/** Output Queue */
+/** Output frame queue */
 static struct LIST_ListDC queue;
 
-/** Currently produced frame, for debugging purposes */
+/** Currently produced frame, mainly for debugging purposes */
 static struct FrameLink * newFrame;
-/** Currently consumed frame, for debugging purposes */
+/** Currently consumed frame, mainly for debugging purposes */
 static struct FrameLink * currentFrame;
 
-/** Mutex */
+/** Queue and Pool Mutex */
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-/** Reader condition (for listOut) */
+/** Reader condition (for queue) */
 static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
-static uint32_t cfgStatBacklog;
-static uint32_t cfgDynBacklog;
-static uint32_t cfgDynIncrement;
-static bool cfgHistory;
-static bool cfgGC;
-static uint32_t cfgGCInterval;
-static uint32_t cfgGCLevel;
+/** Configuration */
+struct Configuration {
+	/** number of frames in static pool */
+	uint32_t statBacklog;
+	/** number of frames in each dynamic pool */
+	uint32_t dynBacklog;
+	/** number of dynamic pools */
+	uint32_t dynIncrement;
+	/** enable history (if not, no dynamic pools are created and the buffer is
+	 * flushed upon reconnect */
+	bool history;
+	/** enable garbage collector */
+	bool gc;
+	/** garbage collector interval */
+	uint32_t gcInterval;
+	/** garbage collector threshold */
+	uint32_t gcLevel;
+};
+
+/** Configuration */
+static struct Configuration cfg;
+
+/** Statistics */
+static struct BUFFER_Statistics stats;
 
 static bool construct();
 static void destruct();
 static void mainloop();
 static void resetStats();
 static void cfgFix(const struct CFG_Section * sect);
-
-static const struct CFG_Section cfg = {
-	.name = "BUFFER",
-	.n_opt = 7,
-	.fix = &cfgFix,
-	.options = {
-		{
-			.name = "StaticBacklog",
-			.type = CFG_VALUE_TYPE_INT,
-			.var = &cfgStatBacklog,
-			.def = {
-				.integer = 200
-			}
-		},
-		{
-			.name = "DynamicBacklog",
-			.type = CFG_VALUE_TYPE_INT,
-			.var = &cfgDynBacklog,
-			.def = {
-				.integer = 1000
-			}
-		},
-		{
-			.name = "DynamicIncrements",
-			.type = CFG_VALUE_TYPE_INT,
-			.var = &cfgDynIncrement,
-			.def = {
-				.integer = 1080
-			}
-		},
-		{
-			.name = "History",
-			.type = CFG_VALUE_TYPE_BOOL,
-			.var = &cfgHistory,
-			.def = { .boolean = false }
-		},
-		{
-			.name = "GC",
-			.type = CFG_VALUE_TYPE_BOOL,
-			.var = &cfgGC,
-			.def = { .boolean = false }
-		},
-		{
-			.name = "GCInterval",
-			.type = CFG_VALUE_TYPE_INT,
-			.var = &cfgGCInterval,
-			.def = { .integer = 120 }
-		},
-		{
-			.name = "GCLevel",
-			.type = CFG_VALUE_TYPE_INT,
-			.var = &cfgGCLevel,
-			.def = { .integer = 2 }
-		},
-	}
-};
-
-static struct BUFFER_Statistics stats;
-
-const struct Component BUF_comp = {
-	.description = PFX,
-	.start = &cfgGC,
-	.onConstruct = &construct,
-	.onDestruct = &destruct,
-	.main = &mainloop,
-	.config = &cfg,
-	.onReset = &resetStats,
-	.dependencies = { NULL }
-};
-
-static void cfgFix(const struct CFG_Section * sect)
-{
-	assert (sect == &cfg);
-	if (cfgStatBacklog < 2) {
-		cfgStatBacklog = 2;
-		LOG_log(LOG_LEVEL_WARN, PFX, "BUFFER.staticBacklog was increased to 2");
-	}
-
-	if (cfgGC && !cfgHistory) {
-		cfgGC = false;
-		LOG_log(LOG_LEVEL_WARN, PFX, "Ignoring BUFFER.GC because "
-			"BUFFER.history is not enabled");
-	}
-}
 
 static bool deployPool(struct Pool * newPool, size_t size);
 static bool createDynPool();
@@ -175,28 +112,115 @@ static bool uncollectPools();
 static void destroyUnusedPools();
 static void gc();
 
-/** Initialize frame buffer. */
+/** Configuration: Descriptor */
+static const struct CFG_Section cfgDesc = {
+	.name = "BUFFER",
+	.n_opt = 7,
+	.fix = &cfgFix,
+	.options = {
+		{
+			.name = "StaticBacklog",
+			.type = CFG_VALUE_TYPE_INT,
+			.var = &cfg.statBacklog,
+			.def = { .integer = 200 }
+		},
+		{
+			.name = "DynamicBacklog",
+			.type = CFG_VALUE_TYPE_INT,
+			.var = &cfg.dynBacklog,
+			.def = { .integer = 1000 }
+		},
+		{
+			.name = "DynamicIncrements",
+			.type = CFG_VALUE_TYPE_INT,
+			.var = &cfg.dynIncrement,
+			.def = { .integer = 1080 }
+		},
+		{
+			.name = "History",
+			.type = CFG_VALUE_TYPE_BOOL,
+			.var = &cfg.history,
+			.def = { .boolean = false }
+		},
+		{
+			.name = "GC",
+			.type = CFG_VALUE_TYPE_BOOL,
+			.var = &cfg.gc,
+			.def = { .boolean = false }
+		},
+		{
+			.name = "GCInterval",
+			.type = CFG_VALUE_TYPE_INT,
+			.var = &cfg.gcInterval,
+			.def = { .integer = 120 }
+		},
+		{
+			.name = "GCLevel",
+			.type = CFG_VALUE_TYPE_INT,
+			.var = &cfg.gcLevel,
+			.def = { .integer = 2 }
+		},
+	}
+};
+
+const struct Component BUF_comp = {
+	.description = PFX,
+	.start = &cfg.gc,
+	.onConstruct = &construct,
+	.onDestruct = &destruct,
+	.main = &mainloop,
+	.config = &cfgDesc,
+	.onReset = &resetStats,
+	.dependencies = { NULL }
+};
+
+/** Configuration: Fix configuration.
+ * @param sect configuration section, should be @cfgDesc
+ */
+static void cfgFix(const struct CFG_Section * sect)
+{
+	assert (sect == &cfgDesc);
+	if (cfg.statBacklog < 2) {
+		cfg.statBacklog = 2;
+		LOG_log(LOG_LEVEL_WARN, PFX, "BUFFER.staticBacklog was increased to 2");
+	}
+
+	if (cfg.gc && !cfg.history) {
+		cfg.gc = false;
+		LOG_log(LOG_LEVEL_WARN, PFX, "Ignoring BUFFER.GC because "
+			"BUFFER.history is not enabled");
+	}
+}
+
+/** Component: Initialize frame buffer.
+ * @return true if buffer could be initialized, false otherwise
+ */
 static bool construct()
 {
-	assert(cfgStatBacklog >= 2);
+	assert(cfg.statBacklog >= 2);
 
+	/* initialize all data structures */
 	LIST_init(&dynPools);
 	LIST_init(&pool);
 	LIST_init(&queue);
 
 	newFrame = currentFrame = NULL;
 
-	dynMaxIncrements = cfgHistory ? cfgDynIncrement : 0;
+	dynMaxPools = cfg.history ? cfg.dynIncrement : 0;
 
-	if (!deployPool(&staticPool, cfgStatBacklog)) {
-		LOG_errno(LOG_LEVEL_ERROR, PFX, "malloc failed");
+	/* deploy static pool */
+	if (!deployPool(&staticPool, cfg.statBacklog)) {
+		LOG_errno(LOG_LEVEL_ERROR, PFX, "malloc failed: could not allocate "
+			"static pool");
 		return false;
 	} else
 		return true;
 }
 
+/** Component: destruct frame buffer. */
 static void destruct()
 {
+	/* delete all dynamic pools */
 	struct LIST_LinkD * l, * n;
 	LIST_foreachSafe(&dynPools, l, n) {
 		struct Pool * dyn = LIST_item(l, struct Pool, list);
@@ -205,6 +229,7 @@ static void destruct()
 	}
 	LIST_init(&dynPools);
 
+	/** delete static pool */
 	free(staticPool.links);
 }
 
@@ -213,16 +238,18 @@ static void destruct()
 static void mainloop()
 {
 	while (true) {
-		sleep(cfgGCInterval);
+		sleep(cfg.gcInterval);
 		pthread_mutex_lock(&mutex);
 		if (LIST_length(&queue) <
-			(LIST_length(&dynPools) * cfgDynBacklog) / cfgGCLevel) {
+			(LIST_length(&dynPools) * cfg.dynBacklog) / cfg.gcLevel) {
+			/* threshold met -> run garbage collector */
 			gc();
 		}
 		pthread_mutex_unlock(&mutex);
 	}
 }
 
+/** Manually trigger garbage collection */
 void BUF_runGC()
 {
 	pthread_mutex_lock(&mutex);
@@ -230,6 +257,13 @@ void BUF_runGC()
 	pthread_mutex_unlock(&mutex);
 }
 
+/** Run garbage collector: collect all unused frames from the overall pool
+ * to the dynamic pool's collected list. If all frames of the pool have been
+ * collected, delete the pool.
+ * There's a good chance, that at least one frame of at least one dynamic pool
+ * is in use. The chance will decrease dramically if the garbage collector is
+ * running twice after a jiffy.
+ */
 static void gc()
 {
 	++stats.GCruns;
@@ -240,7 +274,7 @@ static void gc()
 	destroyUnusedPools();
 }
 
-/** Flush Buffer queue. This discards all buffered but not processed frames */
+/** Flush buffer queue. This discards all buffered but not processed frames */
 void BUF_flush()
 {
 	pthread_mutex_lock(&mutex);
@@ -249,15 +283,18 @@ void BUF_flush()
 	pthread_mutex_unlock(&mutex);
 }
 
+/** Flush buffer queue if history is disabled. This discards all buffered but
+ * not processed frames */
 void BUF_flushUnlessHistoryEnabled()
 {
-	if (!cfgHistory)
+	if (!cfg.history)
 		BUF_flush();
 }
 
 /** Get a new frame from the pool. Extend the pool if there are more dynamic
- *  pools to get. Discard oldest frame if there is no more dynamic pool
- * \return a frame which is about to be produced
+ *  pools to get. Discard oldest frame if there is no more dynamic pool.
+ * @note always succeeds, never waits
+ * @return a frame which is about to be produced
  */
 static inline struct FrameLink * getFrameFromPool()
 {
@@ -276,7 +313,7 @@ static inline struct FrameLink * getFrameFromPool()
 		++stats.uncollectedPools;
 		stats.discardedCur = 0;
 		ret = LIST_itemSafe(&pool, LIST_shift(&pool), struct FrameLink, list);
-	} else if (LIST_length(&dynPools) < dynMaxIncrements && createDynPool()) {
+	} else if (LIST_length(&dynPools) < dynMaxPools && createDynPool()) {
 		/* pool was empty, but we just created another one */
 #ifdef BUF_DEBUG
 		LOG_logf(LOG_LEVEL_DEBUG, PFX, "Created another pool (%zu/%zu)",
@@ -285,8 +322,8 @@ static inline struct FrameLink * getFrameFromPool()
 		stats.discardedCur = 0;
 		ret = LIST_itemSafe(&pool, LIST_shift(&pool), struct FrameLink, list);
 	} else {
-		/* no more space in the pool and no more pools
-		 * -> sacrifice oldest frame */
+		/* no more frames in the pool and no more pools
+		 *  -> sacrifice oldest frame */
 #if defined(BUF_DEBUG) && BUF_DEBUG > 1
 		LOG_log(LOG_LEVEL_DEBUG, PFX, "Sacrificing oldest frame");
 #endif
@@ -301,8 +338,12 @@ static inline struct FrameLink * getFrameFromPool()
 	return ret;
 }
 
-/** Get a frame from the unused frame pool for the producer in order to fill it.
- * \return new frame
+/** Producer: get a frame from the unused frame pool for the producer in order
+ * to fill it.
+ * If the pool is empty, new pools are allocated up to a configured maximum.
+ * If the maximum is reached, the oldest frame will be discarded.
+ * @note always succeeds, never waits
+ * @return new frame to be filled
  */
 struct OPENSKY_RawFrame * BUF_newFrame()
 {
@@ -315,8 +356,8 @@ struct OPENSKY_RawFrame * BUF_newFrame()
 	return &newFrame->frame;
 }
 
-/** Commit a produced frame to the consumer queue.
- * \param frame filled frame to be delivered to the consumer
+/** Producer: commit a produced frame to the consumer queue.
+ * @param frame filled frame to be delivered to the consumer
  */
 void BUF_commitFrame(struct OPENSKY_RawFrame * frame)
 {
@@ -334,8 +375,8 @@ void BUF_commitFrame(struct OPENSKY_RawFrame * frame)
 	newFrame = NULL;
 }
 
-/** Abort producing a frame and return it to the pool.
- * \param frame aborted frame
+/** Producer: abort producing a frame and return it to the pool.
+ * @param frame aborted frame
  */
 void BUF_abortFrame(struct OPENSKY_RawFrame * frame)
 {
@@ -349,8 +390,9 @@ void BUF_abortFrame(struct OPENSKY_RawFrame * frame)
 	newFrame = NULL;
 }
 
-/** Get a frame from the queue.
- * \return frame
+/** Consumer: get a frame from the queue. Waits (infinitely) if the queue is
+ * empty.
+ * @return frame
  */
 const struct OPENSKY_RawFrame * BUF_getFrame()
 {
@@ -366,15 +408,16 @@ const struct OPENSKY_RawFrame * BUF_getFrame()
 	return &currentFrame->frame;
 }
 
-/** Get a frame from the queue.
- * \param timeout_ms timeout in milliseconds
- * \return frame or NULL on timeout
+/** Consumer: get a frame from the queue with timeout.
+ * @param timeout_ms timeout in milliseconds
+ * @return frame or NULL on timeout
  */
 const struct OPENSKY_RawFrame * BUF_getFrameTimeout(uint_fast32_t timeout_ms)
 {
 	struct timespec ts;
 	struct OPENSKY_RawFrame * ret;
 
+	/* get an absolute point in time from the relative timeout */
 	clock_gettime(CLOCK_REALTIME, &ts);
 	ts.tv_sec += timeout_ms / 1000;
 	ts.tv_nsec += (timeout_ms % 1000) * 1000000;
@@ -389,26 +432,26 @@ const struct OPENSKY_RawFrame * BUF_getFrameTimeout(uint_fast32_t timeout_ms)
 	while (LIST_empty(&queue)) {
 		int r = pthread_cond_timedwait(&cond, &mutex, &ts);
 		if (r == ETIMEDOUT) {
+			/* timeout */
 			ret = NULL;
-			break;
+			goto cleanup;
 		} else if (r) {
 			LOG_errno2(LOG_LEVEL_EMERG, r, PFX,
 				"pthread_cond_timedwait failed");
 		}
 	}
-	if (!LIST_empty(&queue)) {
-		currentFrame = LIST_item(LIST_shift(&queue), struct FrameLink, list);
-		ret = &currentFrame->frame;
-	}
+	currentFrame = LIST_item(LIST_shift(&queue), struct FrameLink, list);
+	ret = &currentFrame->frame;
+cleanup:
 	CLEANUP_POP();
 
 	return ret;
 }
 
-/** Put a frame back into the pool.
- * \note Called by the consumer after a frame is consumed in order to put the
- *   frame into the pool again.
- * \param frame frame to be put, must be the last one returned by one of
+/** Consumer: put a frame back into the pool.
+ * @note Called after a frame is consumed in order to put the frame into the
+ *  pool again.
+ * @param frame frame to be put, must be the last one returned by one of
  *  BUF_getFrame() or BUF_getFrameTimeout()
  */
 void BUF_releaseFrame(const struct OPENSKY_RawFrame * frame)
@@ -422,11 +465,11 @@ void BUF_releaseFrame(const struct OPENSKY_RawFrame * frame)
 	pthread_mutex_unlock(&mutex);
 }
 
-/** Put a frame back into the queue.
- * \note This is meant for unsuccessful behavior of the consumer. It should be
+/** Consumer: Put a frame back into the queue.
+ * @note This is meant for unsuccessful behavior of the consumer. It should be
  *  called if the consumer wants to get that frame again next time when
  *  BUF_getFrame() or BUF_getFrameTimeout() is called.
- * \param frame frame to be put, must be the last one returned by one of
+ * @param frame frame to be put, must be the last one returned by one of
  *  BUF_getFrame() or BUF_getFrameTimeout()
  */
 void BUF_putFrame(const struct OPENSKY_RawFrame * frame)
@@ -441,9 +484,9 @@ void BUF_putFrame(const struct OPENSKY_RawFrame * frame)
 }
 
 /** Deploy a new pool: allocate frames and append them to the overall pool.
- * \param newPool pool to be deployed
- * \param size number of frames in that pool
- * \return true if deployment succeeded, false if allocation failed
+ * @param newPool pool to be deployed
+ * @param size number of frames in that pool
+ * @return true if deployment succeeded, false if allocation failed
  */
 static bool deployPool(struct Pool * newPool, size_t size)
 {
@@ -457,11 +500,12 @@ static bool deployPool(struct Pool * newPool, size_t size)
 	newPool->size = size;
 	LIST_init(&newPool->collect);
 
+	/* establish back links */
 	size_t i;
 	for (i = 0; i < size; ++i)
 		initFrames[i].pool = newPool;
 
-	/* setup frame list */
+	/* append frames to the overall pool */
 	struct LIST_ListDC list = LIST_ListDC_INIT(list);
 	LIST_fromVector(&list, initFrames, list, size);
 	LIST_concatenatePush(&pool, &list);
@@ -470,16 +514,17 @@ static bool deployPool(struct Pool * newPool, size_t size)
 }
 
 /** Create a new pool, deploy it and add it to the list of pools.
- * \return true if operation succeeded, false if allocation failed
+ * @return true if operation succeeded, false if allocation failed
  */
 static bool createDynPool()
 {
+	/* allocate pool */
 	struct Pool * newPool = malloc(sizeof *newPool);
 	if (!newPool)
 		return false;
 
 	/* deployment */
-	if (unlikely(!deployPool(newPool, cfgDynBacklog))) {
+	if (unlikely(!deployPool(newPool, cfg.dynBacklog))) {
 		free(newPool);
 		return false;
 	}
@@ -494,18 +539,16 @@ static bool createDynPool()
 	return true;
 }
 
-/** Collect unused frames from the overall pool to their own pool.
- * \note used for garbage collection
+/** Garbage collector: collect unused frames from the overall pool to their own
+ *  pool.
  */
 static void collectPools()
 {
-	//struct FrameLink * frame, * next;
 #ifdef BUF_DEBUG
 	const size_t prevSize = LIST_length(&pool);
 #endif
 
-	/* iterate over all frames of the pool and keep a pointer to the
-	 *  predecessor */
+	/* iterate over all frames of the pool */
 	struct LIST_LinkD * l, * n;
 	LIST_foreachSafe(&pool, l, n) {
 		struct FrameLink * frame = LIST_item(l, struct FrameLink, list);
@@ -523,8 +566,8 @@ static void collectPools()
 #endif
 }
 
-/** Revert effects of the garbage collection.
- * \return true if a dynamic pool was uncollected
+/** Garbage collector: revert first stage of the garbage collection.
+ * @return true if a dynamic pool was uncollected
  *  (i.e. there are new elements in the overall pool)
  */
 static bool uncollectPools()
@@ -544,22 +587,21 @@ static bool uncollectPools()
 	return false;
 }
 
-/** Free all fully collected pools.
- * \note used for garbage collection
- */
+/** Garbage collector: free all fully collected pools. */
 static void destroyUnusedPools()
 {
-	struct LIST_LinkD * l, * n;
 #ifdef BUF_DEBUG
 	const size_t prevSize = LIST_length(&dynPools);
 #endif
 
 	/* iterate over all dynamic pools */
+	struct LIST_LinkD * l, * n;
 	LIST_foreachSafe(&dynPools, l, n) {
 		struct Pool * pool = LIST_item(l, struct Pool, list);
 		if (likely(LIST_length(&pool->collect) ==  pool->size)) {
 			/* fully collected pool */
 
+			/* remove pool from the list of pools */
 			LIST_unlink(&dynPools, l);
 
 			/* delete frames */
